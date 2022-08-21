cca61dc9955887d123b47c16af76a5a331aaca7b
Ilya Yurchenkov <yurchenkov.99@gmail.com>
2022-08-21 10:43:14 +0300

    Task_3

---
+++ Задание_3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+
+int size=ReadInt("Введите размер массива: ");
+int [] array = new int [size];
+
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(int.MinValue,int.MaxValue);
+}
+
+PrintArray(array);
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
