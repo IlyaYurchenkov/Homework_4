079be9f5f8ab7e8e83766a607f86324c86208117
Ilya Yurchenkov <yurchenkov.99@gmail.com>
2022-08-21 09:50:32 +0300

    Task_1

---
+++ Задание_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B
+
+Console.Clear();
+int numberA = ReadInt("Введите число A: ");
+int numberB = ReadInt("Введите число B: ");
+int step = numberA;
+
+for (int i = 1; i < numberB; i++)
+    {
+        step = step * numberA;
+    }   
+
+Console.WriteLine(step);
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
